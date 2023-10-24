0a9eb446012bda1a902d2414b5689c32e923f9b4
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-10-24 22:26:51 +0300

    Add files via upload

--- StartUp.cs
+++ StartUp.cs
@@ -12,41 +12,36 @@ namespace SoftUni
             //instantiate DbContext
             using SoftUniContext context = new SoftUniContext();
 
-            //printing info about employees on the console by calling GetEmployeesFullInformation method
-            Console.WriteLine(GetEmployeesFullInformation(context));
+            //printing info about employees on the console by calling GetEmployeesWithSalaryOver50000 method
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
         }
 
-                                                      //passing DbContext parameter
-        public static string GetEmployeesFullInformation(SoftUniContext context)
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
         {
-          //turning needed info about employee into a collection using anonymous object
-          //using less data
+            //turning needed info about employee into a collection using anonymous object
+            //using less data
             var info = context.Employees
                 .Select(e => new
                 {
                     Firstname = e.FirstName,
-                    Midlename = e.MiddleName,
-                    Lastname = e.LastName,
-                    Jobtitle = e.JobTitle,
-                    Salary = e.Salary,
-                    Id = e.EmployeeId
+                    Salary = e.Salary
                 })
+                .Where(e => e.Salary > 50000)
                 .ToList();
 
             //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
-                                   //ordering elements directly in the foreach loop
-            foreach (var employee in info.OrderBy(e => e.Id))
+            //ordering elements directly in the foreach loop
+            foreach (var employee in info.OrderBy(e => e.Firstname))
             {
                 //using AppendLine so that every printing to be on separate line
-                sb.AppendLine($"{employee.Firstname} {employee.Lastname} {employee.Midlename} {employee.Jobtitle} {employee.Salary:f2}");
+                sb.AppendLine($"{employee.Firstname} - {employee.Salary:f2}");
             }
 
             //using Trim() to get rid of white spaces
             return sb.ToString().Trim();
         }
-
-        
     }
 }

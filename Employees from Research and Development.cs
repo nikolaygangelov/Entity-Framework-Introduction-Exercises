ea9f08dfe7796a7a5d89dc7746a8e97e582e07a7
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-10-25 12:31:55 +0300

    Update Employees from Research and Development.cs

--- Employees from Research and Development.cs
+++ Employees from Research and Development.cs
@@ -16,66 +16,6 @@ namespace SoftUni
             Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
         }
 
-        //Problem 3
-                                                      //passing DbContext parameter
-        public static string GetEmployeesFullInformation(SoftUniContext context)
-        {
-          //turning needed info about employee into a collection using anonymous object
-          //using less data
-            var info = context.Employees
-                .Select(e => new
-                {
-                    Firstname = e.FirstName,
-                    Midlename = e.MiddleName,
-                    Lastname = e.LastName,
-                    Jobtitle = e.JobTitle,
-                    Salary = e.Salary,
-                    Id = e.EmployeeId
-                })
-                .ToList();
-
-            //using StringBuilder to gather all info in one string
-            StringBuilder sb = new StringBuilder();
-
-                                   //ordering elements directly in the foreach loop
-            foreach (var employee in info.OrderBy(e => e.Id))
-            {
-                //using AppendLine so that every printing to be on separate line
-                sb.AppendLine($"{employee.Firstname} {employee.Lastname} {employee.Midlename} {employee.Jobtitle} {employee.Salary:f2}");
-            }
-
-            //using Trim() to get rid of white spaces
-            return sb.ToString().Trim();
-        }
-
-        //Problem 4
-        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
-        {
-            //turning needed info about employee into a collection using anonymous object
-            //using less data
-            var info = context.Employees
-                .Select(e => new
-                {
-                    Firstname = e.FirstName,
-                    Salary = e.Salary
-                })
-                .Where(e => e.Salary > 50000)
-                .ToList();
-
-            //using StringBuilder to gather all info in one string
-            StringBuilder sb = new StringBuilder();
-
-            //ordering elements directly in the foreach loop
-            foreach (var employee in info.OrderBy(e => e.Firstname))
-            {
-                //using AppendLine so that every printing to be on separate line
-                sb.AppendLine($"{employee.Firstname} - {employee.Salary:f2}");
-            }
-
-            //using Trim() to get rid of white spaces
-            return sb.ToString().Trim();
-        }
-
         //Problem 5
         public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
         {
